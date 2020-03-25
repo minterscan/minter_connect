85eaab78ee78ae309e93def3cff88d65f6c83441
whitesunset <chester.work@gmail.com>
2020-03-25 16:52:44 +0100

    Fix package.json

--- dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import MinterConnect from './MinterConnect';
-import { MinterLinkEvent } from './model';
-export { MinterLinkEvent };
-export default MinterConnect;
-//# sourceMappingURL=index.js.map
